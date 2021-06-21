b32383e7985d1544818b04c5960a71c70d242537
mak <faren5128@gmail.com>
2021-06-21 19:57:36 +0300

    commit 1.py new file

--- new_csv/1.py
+++ new_csv/1.py
@@ -1,16 +1,15 @@
 a = [1, 4, 6, 2, 7, 8, 9, 10, 17, 13, 1, 3, 4]
 
+new_arry = []
 c = []
 b = []
+j = 0
 for li in a:
     if li < 10:
         b.append(li)
-print(b)
-
-for li1 in a:
-    if li1 > 10:
-        c.append(li1)
-print(c)
-
-a = [i * 10 for i in a]
-print(a)
+    elif li > 10:
+        c.append(li)
+    if j < len(a):
+        new_arry.append(a[j] * 10)
+        j += 1
+print(b, c, new_arry)
